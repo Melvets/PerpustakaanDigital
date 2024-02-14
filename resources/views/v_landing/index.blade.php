07e2348dcab44594b3aa003c055df35093e245c4
Camela Devs <camelaputri1@gmail.com>
2024-02-14 22:14:10 +0700

    add: landing page (hero)

--- resources/views/v_landing/index.blade.php
+++ resources/views/v_landing/index.blade.php
@@ -1,9 +1,13 @@
 @extends('v_landing.layouts.main')
 
 @section('container')
-
-    <section class="container">
-        <h1>Hello World</h1>
+    <!-- Hero Section start -->
+    <section class="hero_" id="home">
+        <div class="position-absolute top-50 end-0 translate-middle-y text_" style="margin-right: 8rem">
+            <h1 class="fw-bold lh-1" style="font-size: 5em; color: #fafafa; "><span class="mb-0" style="color: #ffcc00">SKANIDA</span><br><span class="mb-0" style="font-size: 0.8em">Perpustakaan</span></h1>
+            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, enim.</p>
+            <a href="#" class="btn fw-bold mt-2 pt-2 pb-2 ps-4 pe-4" style="background-color: #ffcc00">Gabung sekarang! <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M15 16l4 -4" /><path d="M15 8l4 4" /></svg></a>
+        </div>
     </section>
-
+    <!-- Hero Section end -->
 @endsection
